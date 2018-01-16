d372d6b8db330435d6f6c475f305ac7b984457fe
Martin Olejar <martin.olejar@gmail.com>
2018-01-17 00:50:23 +0100

    Fix "init" file

--- src/uboot/__init__.py
+++ src/uboot/__init__.py
@@ -13,27 +13,31 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+from .common import EnumArchType, EnumOsType, EnumImageType, EnumCompressionType
+from .old_image import StdImage, FwImage, ScriptImage, MultiImage, get_img_type, new_img, parse_img
+from .fdt_image import FdtImage
+from .env_image import EnvImgOld
+from .env_blob import EnvBlob
 
-from .uenv import EnvBlob, EnvImgOld
-from .uimg import StdImage, FwImage, ScriptImage, MultiImage, \
-                  OSType, ARCHType, IMGType, COMPRESSType, get_img_type, new_img, parse_img
-
-__author__ = 'Martin Olejar <martin.olejar@gmail.com>'
-__version__ = '0.0.7'
-__status__ = 'Development'
+__author__  = "Martin Olejar"
+__contact__ = "martin.olejar@gmail.com"
+__version__ = "0.0.8"
+__license__ = "Apache 2.0"
+__status__  = "Development"
 __all__ = [
     # Classes
     'EnvBlob',
     'EnvImgOld',
+    'FdtImage',
     'StdImage',
     'FwImage',
     'ScriptImage',
     'MultiImage',
     # Enums
-    'OSType',
-    'ARCHType',
-    'IMGType',
-    'COMPRESSType',
+    'EnumOsType',
+    'EnumArchType',
+    'EnumImageType',
+    'EnumCompressionType',
     # Methods
     'get_img_type',
     'new_img',
