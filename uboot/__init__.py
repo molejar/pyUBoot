b1dc2f77f20680cc328870797be14afdbb927b66
Martin Olejar <martin.olejar@nxp.com>
2019-07-21 23:55:25 +0200

    Set version to 0.1.1

--- uboot/__init__.py
+++ uboot/__init__.py
@@ -21,7 +21,7 @@ from .env_blob import EnvBlob
 
 __author__  = "Martin Olejar"
 __contact__ = "martin.olejar@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "Apache 2.0"
 __status__  = "Development"
 __all__ = [
